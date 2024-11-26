54991f755bc37ceada5ed964e870b798f0b82ad8
Sutirtha Bagchi <subagchi@microsoft.com>
2024-11-26 10:28:50 -0500

    updated terraform code

--- 03-TerCommBas/terraform-manifests/resgrp.tf
+++ 03-TerCommBas/terraform-manifests/resgrp.tf
@@ -12,10 +12,11 @@ terraform {
 # Configure the Microsoft Azure Provider
 provider "azurerm" {
   features {}
+  subscription_id = "704b9446-0d66-45f8-bbf1-c42b2cd62d38"
 }
 
 # Create Resource Group 
-resource "azurerm_resource_group" "my_demo_rg1" {
+resource "azurerm_resource_group" "RG-terraform" {
   location = "eastus"
-  name = "my-demo-rg1"  
+  name = "RG-terraform"  
 }
