bf6a11e5fd53f554438dc7a61a4d8144f3f5f816
Sutirtha Bagchi <subagchi@microsoft.com>
2025-01-02 09:45:24 -0500

    updated terraform code

--- 54-TerCloGHInteg/ter-man/c1-versions.tf
+++ 54-TerCloGHInteg/ter-man/c1-versions.tf
@@ -3,27 +3,25 @@ terraform {
   required_version = ">= 1.0.0"
   required_providers {
     azurerm = {
-      source = "hashicorp/azurerm"
-      version = ">= 2.0" 
+      source  = "hashicorp/azurerm"
+      version = ">= 3.116, < 5"
     }
     random = {
-      source = "hashicorp/random"
+      source  = "hashicorp/random"
       version = ">= 3.0"
     }
   }
 }
 
-# Provider Block
 provider "azurerm" {
- features {}          
+  features {}
+  subscription_id = "704b9446-0d66-45f8-bbf1-c42b2cd62d38"
 }
 
 # Random String Resource
 resource "random_string" "myrandom" {
-  length = 6
-  upper = false 
+  length  = 6
+  upper   = false
   special = false
-  number = false   
+  numeric = false
 }
-
-
--- 54-TerCloGHInteg/ter-man/c5-virtual-network.tf
+++ 54-TerCloGHInteg/ter-man/c5-virtual-network.tf
@@ -1,18 +1,41 @@
 # Create Virtual Network
 resource "azurerm_virtual_network" "myvnet" {
   name                = local.vnet_name
-  address_space       = ["10.0.0.0/16"]
+  address_space       = ["10.1.0.0/16"]
   location            = azurerm_resource_group.myrg.location
   resource_group_name = azurerm_resource_group.myrg.name
-  tags = local.common_tags
+  tags                = local.common_tags
 }
 
+# create network security group
+resource "azurerm_network_security_group" "mynsg" {
+  name                = "nsg-my-home-3389-22"
+  location            = azurerm_resource_group.myrg.location
+  resource_group_name = azurerm_resource_group.myrg.name
+
+  security_rule {
+    name                       = "myhomeaccess3389-22"
+    priority                   = 100
+    direction                  = "Inbound"
+    access                     = "Allow"
+    protocol                   = "Tcp"
+    source_port_range          = "*"
+    destination_port_ranges    = ["3389", "22"]
+    source_address_prefix      = "100.35.206.242"
+    destination_address_prefix = "VirtualNetwork"
+  }
+}
 # Create Subnet
 resource "azurerm_subnet" "mysubnet" {
   name                 = local.snet_name
   resource_group_name  = azurerm_resource_group.myrg.name
   virtual_network_name = azurerm_virtual_network.myvnet.name
   address_prefixes     = ["10.0.2.0/24"]
+}
+# create a nsg association
+resource "azurerm_subnet_network_security_group_association" "subnet-nsg" {
+  subnet_id                 = azurerm_subnet.mysubnet.id
+  network_security_group_id = azurerm_network_security_group.mynsg.id
 }
 
 # Create Public IP Address
@@ -21,8 +44,8 @@ resource "azurerm_public_ip" "mypublicip" {
   resource_group_name = azurerm_resource_group.myrg.name
   location            = azurerm_resource_group.myrg.location
   allocation_method   = "Static"
-  domain_name_label = "app1-${var.environment}-${random_string.myrandom.id}"
-  tags = local.common_tags
+  domain_name_label   = "app1-${var.environment}-${random_string.myrandom.id}"
+  tags                = local.common_tags
 }
 
 # Create Network Interface
@@ -35,7 +58,7 @@ resource "azurerm_network_interface" "myvmnic" {
     name                          = "internal"
     subnet_id                     = azurerm_subnet.mysubnet.id
     private_ip_address_allocation = "Dynamic"
-    public_ip_address_id = azurerm_public_ip.mypublicip.id 
+    public_ip_address_id          = azurerm_public_ip.mypublicip.id
   }
   tags = local.common_tags
 }
